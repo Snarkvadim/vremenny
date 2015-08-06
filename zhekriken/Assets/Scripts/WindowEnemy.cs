34f15621a84c1b483f4b7fa66ce042ef28a5227a
Gasisin <andis1985@mail.ru>
2015-08-06 21:42:23 +0300

    Explosion

---
+++ zhekriken/Assets/Scripts/WindowEnemy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class WindowEnemy : MonoBehaviour{
+    private Animator anim;
+
+    private void Awake(){
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player") {
+            anim.SetBool("Explose", true);
+        }
+    }
+   
+}
