43948f7540925ca0f4a88274f010d4eb960be717
Gasisin <andis1985@mail.ru>
2015-09-01 14:03:48 +0300

    some fix

---
+++ zhekriken/Assets/Scripts/FpsCounter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FpsCounter : MonoBehaviour {
+
+	private const float updateInterval = 0.5f;
+	
+	private float accum = 0.0f; // FPS accumulated over the interval
+	private int frames = 0; // Frames drawn over the interval
+	private float timeleft; // Left time for current interval
+	private Text text;
+
+	void Start () {
+		timeleft = updateInterval;
+		text = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeleft -= Time.deltaTime;
+		accum += Time.timeScale/Time.deltaTime;
+		++frames;
+		
+		// Interval ended - update GUI text and start new interval
+		if( timeleft <= 0.0f ) {
+			// display two fractional digits (f2 format)
+			text.text = "FPS: " + (accum/frames);
+			timeleft = updateInterval;
+			accum = 0.0f;
+			frames = 0;
+		}	
+	}
+}
