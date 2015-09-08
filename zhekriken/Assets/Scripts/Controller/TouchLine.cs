9fcb70b00f07db0d5b0a306be8c1d8c38668fc93
Gasisin <andis1985@mail.ru>
2015-09-09 01:00:17 +0300

    Line by touch

--- zhekriken/Assets/Scripts/Controller/TouchLine.cs
+++ zhekriken/Assets/Scripts/Controller/TouchLine.cs
@@ -14,78 +14,57 @@ public class TouchLine : MonoBehaviour{
     private LineRenderer lineRenderer;
     private bool mouseDown;
 
+    private Dictionary<int, LineRenderer> lineRenderers = new Dictionary<int, LineRenderer>();
+    private Dictionary<int, List<Vector3>> linePositions = new Dictionary<int, List<Vector3>>();
+    private Dictionary<int, GameObject> lineGOs = new Dictionary<int, GameObject>(); 
+
     private void Start(){
-        lineGO = new GameObject("Line");
-        lineRenderer = lineGO.AddComponent<LineRenderer>();
-//        lineRenderer = lineGO.GetComponent<LineRenderer>();
-        lineRenderer.sortingLayerName = "Front";
-        lineRenderer.material = new Material(Shader.Find("Mobile/Particles/Additive"));
-        lineRenderer.SetColors(c1, c2);
-        lineRenderer.SetWidth(0.1F, 0);
-        lineRenderer.SetVertexCount(1);
-        mouseDown = false;
-        linePos = new List<Vector3>();
-        Debug.Log(Camera.main);
+        InputAggregator.Instance.OnNewTouchEvent += OnNewTouchEvent;
+        InputAggregator.Instance.OnTouchMoveEvent += OnTouchMoveEvent;
+        InputAggregator.Instance.OnTouchEndedEvent += OnTouchEndedEvent;
     }
 
-    private void Update(){
-        if (!mouseDown && Input.GetMouseButtonDown(0)){
-            mouseDown = true;
-        }
-        if (mouseDown){
-            if (i < maxLine){
-                lineRenderer.SetVertexCount(i + 1);
-                var mPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _zPos);
-                linePos.Add(mPosition);
-                lineRenderer.SetPosition(i, Camera.main.ScreenToWorldPoint(mPosition));
-                i++;
+    private void OnTouchEndedEvent(int touchId) {
+        lineRenderers[touchId].SetVertexCount(0);
+        i = 0;
+        linePositions[touchId].Clear();
+    }
+
+
+    private void OnTouchMoveEvent(int touchId, Vector3 position){
+        if (i < maxLine) {
+            lineRenderers[touchId].SetVertexCount(i + 1);
+            var mPosition = new Vector3(position.x, position.y, _zPos);
+            linePositions[touchId].Add(mPosition);
+            lineRenderers[touchId].SetPosition(i, Camera.main.ScreenToWorldPoint(mPosition));
+            i++;
+        } else {
+            for (int b = 0; b < linePositions[touchId].Count; b++) {
+                lineRenderers[touchId].SetPosition((b), Camera.main.ScreenToWorldPoint(linePositions[touchId][b]));
             }
-            else{
-                for (int b = 0; b < linePos.Count; b++){
-                    lineRenderer.SetPosition((b), Camera.main.ScreenToWorldPoint(linePos[b]));
-                }
-                var mPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _zPos);
-                lineRenderer.SetPosition(maxLine - 1, Camera.main.ScreenToWorldPoint(mPosition));
-                linePos.RemoveAt(0);
-                linePos.Add(mPosition);
-            }
-        }
-        if (mouseDown && Input.GetMouseButtonUp(0)){
-            lineRenderer.SetVertexCount(0);
-            i = 0;
-            linePos.Clear();
-            mouseDown = false;
-        }
-        if (Input.touchCount > 0){
-            Touch touch = Input.GetTouch(0);
-
-            if (touch.phase == TouchPhase.Moved){
-                lineRenderer.SetVertexCount(i + 1);
-                var mPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _zPos);
-                lineRenderer.SetPosition(i, Camera.main.ScreenToWorldPoint(mPosition));
-                i++;
-
-                /* Add Collider */
-
-                var bc = lineGO.AddComponent<BoxCollider>();
-                bc.transform.position = lineRenderer.transform.position;
-                bc.size = new Vector3(0.1f, 0.1f, 0.1f);
-            }
-
-            if (touch.phase == TouchPhase.Ended){
-                /* Remove Line */
-
-                lineRenderer.SetVertexCount(0);
-                i = 0;
-
-                /* Remove Line Colliders */
-
-                BoxCollider[] lineColliders = lineGO.GetComponents<BoxCollider>();
-
-                foreach (BoxCollider b in lineColliders){
-                    Destroy(b);
-                }
-            }
+            var mPosition = new Vector3(position.x, position.y, _zPos);
+            lineRenderers[touchId].SetPosition(maxLine - 1, Camera.main.ScreenToWorldPoint(mPosition));
+            linePositions[touchId].RemoveAt(0);
+            linePositions[touchId].Add(mPosition);
         }
     }
+
+    private void OnNewTouchEvent(int touchId){
+//        Debug.LogError("NewTouch id - "+touchId);
+        if (!lineGOs.ContainsKey(touchId)){
+            lineGOs.Add(touchId, new GameObject("Line"+touchId));
+        }
+        if (!lineRenderers.ContainsKey(touchId)){
+            lineRenderers.Add(touchId, lineGOs[touchId].AddComponent<LineRenderer>());
+            lineRenderers[touchId].sortingLayerName = "Front";
+            lineRenderers[touchId].material = new Material(Shader.Find("Mobile/Particles/Additive"));
+            lineRenderers[touchId].SetColors(c1, c2);
+            lineRenderers[touchId].SetWidth(0.1F, 0);
+            lineRenderers[touchId].SetVertexCount(1);
+        }
+        if (!linePositions.ContainsKey(touchId)){
+            linePositions.Add(touchId, new List<Vector3>());
+        }
+    }
+
 }
---
+++ zhekriken/Assets/Scripts/Events/InputAgregator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputAggregator : MonoBehaviour {
+    public Action<Vector3> OnScrollEvent; //deltaPosition Vector3
+    public Action<float> OnZoomEvent; //deltaPosition Vector3
+    public Action<int> OnNewTouchEvent; //touch number
+    public Action<int, Vector3> OnTouchMoveEvent; //touch number
+    public Action<int> OnTouchEndedEvent; //touch number
+
+    private EventSystem eventSystem;
+
+    private bool is_mouse_button_down;
+    private Vector3 previous_position;
+
+    private static InputAggregator instance;
+
+    public static InputAggregator Instance {
+        get {
+            if (instance == null) {
+                var go = new GameObject("InputAggregator", typeof(InputAggregator));
+                instance = go.GetComponent<InputAggregator>();
+                DontDestroyOnLoad(go);
+            }
+            return instance;
+        }
+    }
+
+    private void Start() {
+        eventSystem = EventSystem.current;
+    }
+
+    private void Update() {
+        onTouchEvent();
+        onMouseEvent();
+    }
+
+    private void onTouchEvent() {
+        if (Input.touchCount > 0){
+            foreach (var touch in Input.touches){
+                if (touch.phase == TouchPhase.Began){
+                    OnNewTouchEvent(touch.fingerId);
+                }
+                if (touch.phase == TouchPhase.Moved){
+                    OnTouchMoveEvent(touch.fingerId, touch.position);
+                }
+                if (touch.phase == TouchPhase.Ended) {
+                    OnTouchEndedEvent(touch.fingerId);
+                }
+            }
+        }
+    }
+
+    private void onMouseEvent() {
+        if (Input.GetMouseButtonDown(0)){
+            if (eventSystem.currentSelectedGameObject != null)
+                return;
+            is_mouse_button_down = true;
+            OnNewTouchEvent(0);
+        }
+        if (Input.GetMouseButtonUp(0)){
+            is_mouse_button_down = false;
+            OnTouchEndedEvent(0);
+        }
+        if (is_mouse_button_down) {
+            OnTouchMoveEvent(0, Input.mousePosition);
+        }
+
+    }
+}
